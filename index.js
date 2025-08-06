cf10fd0f0c82abd50746630ff5847231d68b1efe
Aleksandra Umnykh <43292102+sashaumnykh@users.noreply.github.com>
2025-08-06 18:22:48 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Pop It Bubble</title>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <style>
+      html, body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        overflow: hidden;
+      }
+      iframe {
+        border: none;
+        width: 100%;
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- Встроим твою игру в iframe -->
+    <iframe src="https://sashaumnykh.github.io/bubble-wrap/"></iframe>
+  </body>
+</html>
